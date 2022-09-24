d0d0d8b0aad51b0c117b2a935259015e6633fe73
Michael Wood <michaelwood33311@icloud.com>
2022-09-24 09:29:17 -0400

    added function to return available linode instance types sorted from cheapest to most expensive

--- linode.js
+++ linode.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
-
 const linodeToken = process.env.LINODE_TOKEN;
+const publicKey = process.env.PUBLIC_KEY;
+console.log(publicKey);
 
 const axios = require('axios');
+
+const prettyJSON = obj => {
+    return JSON.stringify(obj, null, 4);
+}
+
+const show = obj => {
+    console.log(prettyJSON(obj));
+}
 
 const linodeGet = (url, params = {}) => {
     return new Promise((resolve, reject) => {
@@ -35,9 +44,33 @@ const linodeList = async (page = 1, page_size = 100) => {
     })
 }
 
+// returns linode types sorted cheapest to most expensive
+const linodeTypes = async () => {
+    let types = await linodeGet('https://api.linode.com/v4/linode/types');
+
+    let { data } = types;
+
+    return data.sort((a, b) => a.price.monthly = b.price.monthly);
+}
+
+// const linodeCreateInstance = async params => {
+    
+// }
+
 const test = async () => {
-    let list = await linodeRegionsList();
-    console.log(list);
+    // let list = await linodeCreateInstance({
+    //     authorized_keys: [publicKey],
+    //     backups_enabled: false,
+    //     label: 'test-linode',
+    //     private_ip: false,
+    //     region: 'us-east',
+    //     root_pass: 'simpleTest123'
+
+    // });
+    
+    let types = await linodeTypes();
+    show(types);
+
 }
 
 test();
