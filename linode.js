9928d52228a92f0c4a85e6a0ebd73d30ad7d5ed7
Michael Wood <michaelwood33311@icloud.com>
2022-09-24 09:01:35 -0400

    added linodeGet wrapper function

---
+++ linode.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const linodeToken = process.env.LINODE_TOKEN;
+
+const axios = require('axios');
+
+const linodeGet = (url, params = {}) => {
+    return new Promise((resolve, reject) => {
+        let request = {
+            url,
+            params,
+            method: 'get',
+            headers: {
+                Authorization: `Bearer ${linodeToken}`
+            }
+        };
+        axios(request)
+        .then(response => resolve(response.data))
+        .catch(error => reject(error));
+    });
+}
+
