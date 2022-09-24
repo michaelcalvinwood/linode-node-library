12c3704194ce071446d61185614c5b380b12f3e5
Michael Wood <michaelwood33311@icloud.com>
2022-09-24 10:12:05 -0400

    added functions for creating and deleting linode instances

--- linode.js
+++ linode.js
@@ -14,19 +14,45 @@ const show = obj => {
 }
 
 const linodeGet = (url, params = {}) => {
+    let request = {
+        url,
+        params,
+        method: 'get',
+        headers: {
+            Authorization: `Bearer ${linodeToken}`
+        }
+    };
     return new Promise((resolve, reject) => {
-        let request = {
-            url,
-            params,
-            method: 'get',
-            headers: {
-                Authorization: `Bearer ${linodeToken}`
-            }
-        };
         axios(request)
         .then(response => resolve(response.data))
-        .catch(error => reject(error));
+        .catch(error => {
+            console.error(error);
+            reject(error);}
+        );
     });
+}
+
+const linodePost = async (url, params) => {
+    let request = {
+        url,
+        method: 'post',
+        data: params,
+        headers: {
+            Authorization: `Bearer ${linodeToken}`
+        }
+    }
+    request.headers['Content-Type'] = 'application/json';
+
+    return new Promise((resolve, reject) => {
+        axios(request)
+        .then(response => resolve(response.data))
+        .catch(error => {
+            console.error(error);
+            reject(error);
+        }    
+        );
+
+    })   
 }
 
 const linodeAccountView = async () => {
@@ -44,6 +70,10 @@ const linodeList = async (page = 1, page_size = 100) => {
     })
 }
 
+const linodeImages = async () => {
+    return await linodeGet('https://api.linode.com/v4/images');
+}
+
 // returns linode types sorted cheapest to most expensive
 const linodeTypes = async () => {
     let types = await linodeGet('https://api.linode.com/v4/linode/types');
@@ -53,24 +83,67 @@ const linodeTypes = async () => {
     return data.sort((a, b) => a.price.monthly = b.price.monthly);
 }
 
-// const linodeCreateInstance = async params => {
+const linodeCreateInstance = async params => {
+    return await linodePost('https://api.linode.com/v4/linode/instances', params);
+}
+
+const linodeDeleteInstance = id => {
+    let request = {
+        url: `https://api.linode.com/v4/linode/instances/${id}`,
+        method: 'delete',
+        headers: {
+            Authorization: `Bearer ${linodeToken}`
+        }
+    }
     
-// }
+    return new Promise((resolve, reject) => {
+        axios(request)
+        .then(response => resolve(response.data))
+        .catch(error => {
+            console.error(error);
+            reject(error);
+        })
+    })
+}
+
+const showImages = async () => {
+    let images = await linodeImages();
+    let info = images.data.map(image => {
+        let data = {};
+        data.id = image.id;
+        data.deprecated = image.deprecated;
+        return data;
+    })
+    show (info);
+    
+}
 
 const test = async () => {
-    // let list = await linodeCreateInstance({
-    //     authorized_keys: [publicKey],
-    //     backups_enabled: false,
-    //     label: 'test-linode',
-    //     private_ip: false,
-    //     region: 'us-east',
-    //     root_pass: 'simpleTest123'
-
-    // });
+    let deleteInfo = await linodeDeleteInstance('38986936');
+    show(deleteInfo);
     
-    let types = await linodeTypes();
-    show(types);
+    return;
+
+    let instanceTypes = await linodeTypes();
+    
+    let newLinode = await linodeCreateInstance({
+        image: 'linode/ubuntu20.04',
+        authorized_keys: [publicKey],
+        backups_enabled: false,
+        label: 'test-linode',
+        private_ip: false,
+        region: 'us-east',
+        root_pass: 'simpleTest123',
+        type: instanceTypes[0].id
+
+    });
+    
+    show(newLinode);
 
 }
 
-test();
+try {
+    test();
+} catch (e) {
+    console.error(e);
+}
